d67ad6fe63dbbf357564997a773e67e786c5359e
WhoAteDaCake <doitsimplefy@gmail.com>
2018-12-14 20:22:03 +0000

    use NAME variable in script

--- twitter-scraper/deploy.sh
+++ twitter-scraper/deploy.sh
@@ -1,7 +1,7 @@
 REPO_TAG=whoatedacakeidid/twitter-scraper:latest
 NAME=twitter-scraper
 
-docker build -t twitter-scraper .
-ID=$(docker images -q twitter-scraper)
+docker build -t $NAME .
+ID=$(docker images -q $NAME)
 docker tag $ID $REPO_TAG
 docker push $REPO_TAG
