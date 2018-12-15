c8edc585d5d791bc81838f0c9efbfdcfe84154f1
WhoAteDaCake <doitsimplefy@gmail.com>
2018-12-15 11:01:55 +0000

    update postgresql docker configuration

--- database/postgres/postgres.sh
+++ database/postgres/postgres.sh
@@ -1,6 +1,18 @@
-docker pull postgres
-mkdir -p $HOME/docker/volumes/postgres
+# This should just be used for local deployment
+NAME=whoatedacakeidid/processing-postgres
+ID=$(docker ps -aqf "name=pg-docker")
+#docker stop $ID
+
+VOLUME="$HOME/docker/volumes/postgres"
+
+# To allow export scripts to run again
+sudo rm -rf $VOLUME
+mkdir -p $VOLUME
 docker run --rm --name pg-docker \
+	-e POSTGRES_USER=postgres \
 	-e POSTGRES_PASSWORD=docker \
-	-d -p 5432:5432 \
-	-v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
+	-e POSTGRES_DB=content \
+	-p 5432:5432 \
+	-v $VOLUME:/var/lib/postgresql/data $NAME
+
+echo $(docker ps -aqf "name=pg-docker")
